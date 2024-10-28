0b61ef9c61434c1b136de120093b5a2626c4130a
Jimmy <jimmyliew1@outlook.com>
2024-10-28 22:48:53 +0800

    Quartz sync: Oct 28, 2024, 10:48 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🗃️ My Notes",
+    pageTitle: "🗃️ Jimmy's Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
