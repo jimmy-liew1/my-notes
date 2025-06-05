9452f9b68cd4d440287265922e1c54e2702b8a74
Jimmy <jimmy_scs_19@outlook.com>
2025-06-05 16:48:29 +0800

    Quartz sync: Jun 5, 2025, 4:48 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,10 +15,6 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           Created by <a href="https://jimmy-liew1.github.io/my-notes/">Jimmy Liew</a>, © {year}
-          <!--
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-          -->
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
